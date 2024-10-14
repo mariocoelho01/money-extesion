747fdb41dcb8ef22d922dee77916aa5339076781
Mario <mariocoelho-21@hotmail.com>
2024-10-14 03:25:14 -0300

    finished the first version

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+namespace MoneyExtension.test;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal value = 279.98M;
+        var cents = value.ToCents();
+        
+        Assert.AreEqual(27998, cents);
+    }
+}
